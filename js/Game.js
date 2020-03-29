cf17435be0337f01475e9415cc4512efce332ea4
Adam Cessna <racessna@gmail.com>
2020-03-29 16:58:13 -0500

    createPhrases and getRandomPhrase
    finish both methods

--- js/Game.js
+++ js/Game.js
@@ -1,7 +1,31 @@
 class Game{
     constructor(){
         this.missed = 0;
-        this.phrases = [];
+        this.phrases = this.createPhrases();
         this.activePhrase = null;
+    }
+
+    /**
+    * Creates phrases for use in game
+    * @return {array} An array of phrases that could be used in the game
+    */
+    createPhrases(){
+        return [new Phrase('life is like a box of chocolates'),
+        new Phrase('better late than never'),
+        new Phrase('another day another dollar'),
+        new Phrase('its a trap'),
+        new Phrase('the spoon isnt real')
+        ];
+
+        
+    }
+
+    /**
+    * Selects random phrase from phrases property
+    * @return {Object} Phrase object chosen to be used
+    */
+    getRandomPhrase(){
+        let ranNum = Math.floor(Math.random() * 5);
+        return this.phrases[ranNum]; 
     }
 }
--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
-/* const phrase = new Phrase('Life is like a box of chocolates!');
-const game = new Game(); 
-
-console.log(`Phrase - phrase: ${phrase.phrase}`); */
+const logPhrase = (phrase) => {
+    console.log(`Phrase - phrase: `, phrase.phrase);
+    };
+    const game = new Game();
+    logPhrase(game.getRandomPhrase());
+    logPhrase(game.getRandomPhrase());
+    logPhrase(game.getRandomPhrase());
+    logPhrase(game.getRandomPhrase());
+    logPhrase(game.getRandomPhrase());

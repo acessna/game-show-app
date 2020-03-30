7c41f068b3af066067e02f557cf0e22f395b82f5
Adam Cessna <racessna@gmail.com>
2020-03-29 19:05:55 -0500

    addPhraseToDisplay finished

--- js/Phrase.js
+++ js/Phrase.js
@@ -2,4 +2,19 @@ class Phrase{
     constructor(phrase){
         this.phrase = phrase;
     }
+
+    addPhraseToDisplay(){
+        const phraseLetters = [];
+        for (let i = 0; i < this.phrase.length; i++) {
+            phraseLetters.push(this.phrase.charAt(i)); 
+        };
+        phraseLetters.forEach(letter => {
+            if(letter === ' '){
+                $('#phrase ul').append( '<li class="space"> </li>');
+            }else{
+                $('#phrase ul').append(`<li class="hide letter ${letter}">${letter}</li>`);
+            }
+        });
+
+    }
 }
